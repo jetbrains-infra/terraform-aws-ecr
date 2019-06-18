d10e86cd17dc95ef88d94b9a39927bb98b573eb8
Martín Beauchamp <martin@iterative.capital>
2019-06-18 08:53:39 -0400

    add lifecycle retention policy to registry

--- ecr.tf
+++ ecr.tf
@@ -5,3 +5,26 @@ resource "aws_ecr_repository" "image" {
     Project = "${local.project}"
   }
 }
+
+# Only keep last 30 images
+resource "aws_ecr_lifecycle_policy" "cleanup" {
+  repository = "${aws_ecr_repository.image.name}"
+  policy = << END_OF_POLICY
+{
+    "rules": [
+      {
+        "rulePriority": 1,
+        "description": "Keep last 30 images",
+        "selection": {
+          "tagStatus": "any",
+          "countType": "imageCountMoreThan",
+          "countNumber": 30
+        },
+        "action": {
+          "type": "expire"
+        }
+      }
+    ]
+}
+END_OF_POLICY
+}
